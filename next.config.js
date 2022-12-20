e181360824ff75ab680c7deec733053a9547f640
splashhh-ai-art <120738759+splashhhcc@users.noreply.github.com>
2022-12-20 10:03:48 +0200

    deploy4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'splashhh-home-deploy'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
     minimumCacheTTL: 60,
   },
+
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 module.exports = nextConfig
